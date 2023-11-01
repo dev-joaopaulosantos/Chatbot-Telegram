b78e111c01f2238dba3689d8e9d02d9efb7f6cd5
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-11-01 01:22:52 -0300

    refactor: refatoração nas gunções handlers

--- bot.py
+++ bot.py
@@ -11,26 +11,28 @@ data = json.loads(open('training.json', 'r', encoding='utf-8').read())
 questions = []
 answers = {}
 id_to_answer = {}
-
+codes = []
 
 for row in data:
     for question in row['questions']:
         questions.append(question)
         answers[question] = row['answer']
     id_to_answer[row['id']] = row['answer']
+    codes.append(row['code'])
 
-
-print(questions)
-print("= = = = = = = = = = = = = = = = = = = = = = =")
-print(answers)
+# print(questions)
+# print("= = = = = = = = = = = = = = = = = = =")
+# print(codes)
 
 bot = telebot.TeleBot(os.getenv('TELEGRAM_API_KEY'))
+
 
 def verify(message):
     question = message.text
     best_match = process.extractOne(question, questions)
     if best_match[1] > 80:
         return True
+
 
 def verify_main_menu(message):
     question = message.text
@@ -39,59 +41,47 @@ def verify_main_menu(message):
         return True
 
 
-@bot.message_handler(commands=['subopcao1'])
-def sub_menu_opcao1(message):
-    response = id_to_answer[1]
-    bot.reply_to(message, str(response))
-
-@bot.message_handler(commands=['subopcao2'])
-def sub_menu_opcao1(message):
-    response = id_to_answer[2]
-    bot.reply_to(message, str(response))
-
-@bot.message_handler(commands=['subopcao3'])
-def sub_menu_opcao1(message):
-    response = id_to_answer[3]
-    bot.reply_to(message, str(response))
-
-@bot.message_handler(commands=['subopcao4'])
-def sub_menu_opcao1(message):
-    response = id_to_answer[4]
+def handle_sub_option(message, option_id):
+    response = id_to_answer[option_id]
     bot.reply_to(message, str(response))
 
 
+@bot.message_handler(commands=codes)
+def sub_menu(message):
+    option_id_str = message.text.split('OP')[-1]
+    option_id_int = int(option_id_str)
+    handle_sub_option(message, option_id_int)
 
-@bot.message_handler(commands=['opcao1'])
+
+@bot.message_handler(commands=['01'])
 def menu_opcao1(message):
     text = """
     Você escolheu a opção 1. Aqui estão algumas subopções:
-    /subopcao1 Subopção 1
-    /subopcao2 Subopção 2"""
+    /OP01 Subopção 1
+    /OP02 Subopção 2"""
     bot.reply_to(message, text)
 
 
-
-@bot.message_handler(commands=['opcao2'])
+@bot.message_handler(commands=['02'])
 def menu_opcao2(message):
     text = """
     Você escolheu a opção 2. Aqui estão algumas subopções:
-    /subopcao3 Subopção 3
-    /subopcao4 Subopção 4"""
+    /OP03 Subopção 3
+    /OP04 Subopção 4"""
     bot.reply_to(message, text)
-
 
 
 @bot.message_handler(func=verify_main_menu)
 def main_menu(message):
     text = """
-    Escolha uma opção para continuar (Clique no item):
+*Escolha uma opção para continuar* (Clique no item):
 
-    /opcao1 Como solicitar declaração
-    /opcao2 Informações sobre bolsas
+- /01 Como solicitar declaração
+- /02 Informações sobre bolsas
 
-Ou digite sobre o que você deseja se informar!"""
-    bot.reply_to(message, text)
-
+Ou digite sobre o que você deseja se informar!
+"""
+    bot.reply_to(message, text, parse_mode='Markdown')
 
 
 @bot.message_handler(func=verify)
@@ -100,5 +90,6 @@ def respond(message):
     best_match = process.extractOne(question, questions)
     response = answers[best_match[0]]
     bot.reply_to(message, str(response))
+
 
 bot.polling()
