50dace16659a34b3860052db2278b5f8849564aa
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-11-26 21:40:23 -0300

    config

--- helpers.py
+++ helpers.py
@@ -1,9 +1,17 @@
 # helpers.py
 
 import datetime
+import pytz
 
 def get_salutation():
-    current_hour = datetime.datetime.now().hour
+    # Configurar o fuso horário desejado
+    tz = pytz.timezone('America/Sao_Paulo')  # Substitua pelo seu fuso horário
+
+    # Obter a hora atual no fuso horário especificado
+    current_time = datetime.datetime.now(tz)
+
+    # Extrair a hora do objeto de data e hora
+    current_hour = current_time.hour
 
     if current_hour < 12:
         return 'Bom dia'
